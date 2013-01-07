bbabfff1a8499823509ab3e2995550d6a86c99c0
Danna Aduna <dannaaduna@gmail.com>
2013-01-07 15:50:52 +0800

    Add pid and status commands to cpx_ctl

--- src/cpx_ctl.erl
+++ src/cpx_ctl.erl
@@ -14,5 +14,22 @@ process(["restart"]) ->
 	init:restart(),
 	?RET_SUCCESS;
 
+process(["pid"]) ->
+	io:format("~s~n", [os:getpid()]),
+	?RET_SUCCESS;
+
+process(["status"]) ->
+	{ok, Uptime} = application:get_env(oacd_core, uptime),
+	AgentCount = length(agent_manager:list()),
+	{ok, Queues} = call_queue_config:get_queues(),
+	QueueCount = length(Queues),
+	Plugins = cpx:plugins_running(),
+	io:format("Uptime: ~p~n", [Uptime]),
+	io:format("Number of queues: ~p~n", [QueueCount]),
+	io:format("Number of agents logged in: ~p~n", [AgentCount]),
+	io:format("~nPlugins running:~n"),
+	[io:format("~p~n", [P]) || {P, running} <- Plugins],
+	?RET_SUCCESS;
+
 process(_) ->
 	?RET_INVALID_COMMAND.
