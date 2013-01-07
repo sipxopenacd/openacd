d3c8ed89f213d99637915ac5950099f72ee1de33
Danna Aduna <dannaaduna@gmail.com>
2013-01-07 15:44:18 +0800

    Add stop and restart to cpx_ctl

--- src/cpx_ctl.erl
+++ src/cpx_ctl.erl
@@ -1,7 +1,18 @@
 -module(cpx_ctl).
 -export([process/1]).
 
+-define(RET_SUCCESS, {ok, 0}).
 -define(RET_INVALID_COMMAND, {error, 1}).
+
+process(["stop"]) ->
+	io:format("Stopping openacd~n"),
+	init:stop(),
+	?RET_SUCCESS;
+
+process(["restart"]) ->
+	io:format("Restarting openacd~n"),
+	init:restart(),
+	?RET_SUCCESS;
 
 process(_) ->
 	?RET_INVALID_COMMAND.
