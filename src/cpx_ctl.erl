a7da34c6c5538bbe308c9bf2d781dcb6c2485be8
Danna Aduna <dannaaduna@gmail.com>
2013-01-08 18:05:29 +0800

    Process list-agents command in cpx_ctl

--- src/cpx_ctl.erl
+++ src/cpx_ctl.erl
@@ -1,11 +1,16 @@
 -module(cpx_ctl).
 -export([process/1]).
 
+-include_lib("stdlib/include/qlc.hrl").
+-include("agent.hrl").
+
 -define(RET_SUCCESS, {ok, 0}).
 -define(RET_INVALID_COMMAND, {error, 1}).
 
 -define(PRINT(Fmt), io:format(Fmt, [])).
 -define(PRINT(Fmt, Data), io:format(Fmt, Data)).
+
+-record(ctl_agent, {agent, profile, state, login_time}).
 
 process(["stop"]) ->
 	io:format("Stopping openacd~n"),
@@ -35,6 +40,17 @@ process(["status"]) ->
 	?RET_SUCCESS;
 
 process(["list-agents"]) ->
+	Agents = qlc:e(qlc:q([#ctl_agent{agent=Login, profile=Profile, state=State, login_time=StartTime} || {_, _, #cpx_agent_prop{login=Login, profile=Profile, state=State, start_time=StartTime}} <- gproc:table({l, p})])),
+	lists:foreach(fun(A) ->
+		?PRINT("~-10s", [A#ctl_agent.agent]),
+		?PRINT("~-15s", [A#ctl_agent.profile]),
+		{{Y,M,D}, {H,Mi,S}} = calendar:now_to_local_time(A#ctl_agent.login_time),
+		?PRINT("~4..0B/~2..0B/~2..0B ~2..0B:~2..0B:~2..0B     ", [Y,M,D,H,Mi,S]),
+		case A#ctl_agent.state of
+			available -> ?PRINT("Available~n");
+			{released, {Reason,_,_}} -> ?PRINT("Released: ~s~n", [Reason])
+		end
+	end, Agents),
 	?RET_SUCCESS;
 
 process(["list-queues"]) ->
