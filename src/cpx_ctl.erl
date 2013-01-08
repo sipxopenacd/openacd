d6b1ab488c9621da133c681c2b60689b9881f223
Danna Aduna <dannaaduna@gmail.com>
2013-01-08 17:39:26 +0800

    Define PRINT macro for cpx_ctl

--- src/cpx_ctl.erl
+++ src/cpx_ctl.erl
@@ -4,6 +4,9 @@
 -define(RET_SUCCESS, {ok, 0}).
 -define(RET_INVALID_COMMAND, {error, 1}).
 
+-define(PRINT(Fmt), io:format(Fmt, [])).
+-define(PRINT(Fmt, Data), io:format(Fmt, Data)).
+
 process(["stop"]) ->
 	io:format("Stopping openacd~n"),
 	init:stop(),
@@ -24,11 +27,11 @@ process(["status"]) ->
 	{ok, Queues} = call_queue_config:get_queues(),
 	QueueCount = length(Queues),
 	Plugins = cpx:plugins_running(),
-	io:format("Uptime: ~p~n", [Uptime]),
-	io:format("Number of queues: ~p~n", [QueueCount]),
-	io:format("Number of agents logged in: ~p~n", [AgentCount]),
-	io:format("~nPlugins running:~n"),
-	[io:format("~p~n", [P]) || {P, running} <- Plugins],
+	?PRINT("Uptime: ~p~n", [Uptime]),
+	?PRINT("Number of queues: ~p~n", [QueueCount]),
+	?PRINT("Number of agents logged in: ~p~n", [AgentCount]),
+	?PRINT("~nPlugins running:~n"),
+	[?PRINT("~p~n", [P]) || {P, running} <- Plugins],
 	?RET_SUCCESS;
 
 process(["list-agents"]) ->
