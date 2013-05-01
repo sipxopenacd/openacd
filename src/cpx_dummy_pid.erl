aa7998c8b08c0ad7a0d07bf4a89e318f30febde4
Jan Vincent Liwanag <jvliwanag@gmail.com>
2013-05-01 10:26:32 +0800

    Add cpx_dummy_pid msg/do funs

--- src/cpx_dummy_pid.erl
+++ src/cpx_dummy_pid.erl
@@ -10,14 +10,20 @@
 	start/0,
 	start_link/0,
 	stop/1,
-	stop/2
+	stop/2,
+
+	do/2,
+	has_received/2,
+	watch_for/2
 ]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	terminate/2, code_change/3]).
 
--record(state, {}).
+-record(state, {
+	rcv_log = []
+}).
 
 %% API
 
@@ -35,6 +41,15 @@ stop(Pid) ->
 stop(Pid, Reason) ->
 	gen_server:call(Pid, {stop, Reason}).
 
+do(Pid, F) ->
+	gen_server:call(Pid, {do, F}).
+
+has_received(Pid, Msg) ->
+	gen_server:call(Pid, {has_received, Msg}).
+
+watch_for(Pid, Msg) ->
+	gen_server:call(Pid, {watch_for, Msg}).
+
 %% gen_server callbacks
 
 init([]) ->
@@ -42,6 +57,19 @@ init([]) ->
 
 handle_call({stop, Reason}, _From, St) ->
 	{stop, Reason, ok, St};
+handle_call({do, F}, _From, St) ->
+	{reply, F(), St};
+handle_call({has_received, Msg}, _From, St) ->
+	Reply = lists:member(Msg, St#state.rcv_log),
+	{reply, Reply, St};
+handle_call({watch_for, Msg}, From, St) ->
+	case lists:member(Msg, St#state.rcv_log) of
+		true -> {reply, true, St};
+		_ ->
+			timer:send_after(10, {watch_for_req, Msg, From}),
+			{noreply, St}
+	end;
+
 handle_call(_Request, _From, St) ->
     Reply = ok,
     {reply, Reply, St}.
@@ -49,8 +77,13 @@ handle_call(_Request, _From, St) ->
 handle_cast(_Msg, St) ->
     {noreply, St}.
 
-handle_info(_Info, St) ->
-    {noreply, St}.
+handle_info({watch_for_req, Msg, From}, St) ->
+	Reply = lists:member(Msg, St#state.rcv_log),
+	gen_server:reply(From, Reply);
+handle_info(M, St) ->
+	Log = St#state.rcv_log,
+	St1 = St#state{rcv_log=[M|Log]},
+    {noreply, St1}.
 
 terminate(_Reason, _St) ->
     ok.
@@ -81,6 +114,34 @@ exit_test() ->
 	?M:stop(Pid, bla),
 	?assertNot(is_process_alive(Pid)),
 	?assert(receive {'EXIT', Pid, bla} -> true after 0 -> false end).
+
+do_test() ->
+	Pid = ?M:start_link(),
+	From = self(),
+	?assertEqual(5, cpx_dummy_pid:do(Pid, fun() -> From ! hello, 5 end)),
+	?assert(receive hello -> true after 5 -> false end).
+
+has_received_test() ->
+	Pid = ?M:start_link(),
+	Pid ! a,
+	Pid ! b,
+	timer:sleep(10),
+	?assert(has_received(Pid, a)),
+	?assert(has_received(Pid, b)),
+	?assertNot(has_received(Pid, x)).
+
+watch_for_test_() ->
+	{foreach, fun() -> ?M:start_link() end, fun(Pid) -> catch ?M:stop(Pid) end, [
+		fun(Pid) -> {"already received", fun() ->
+			Pid ! a,
+			timer:sleep(10),
+			?assert(watch_for(Pid, a))
+		end} end,
+		fun(Pid) -> {"receive within", fun() ->
+			spawn_link(fun() -> timer:sleep(100), Pid ! b end),
+			?assert(watch_for(Pid, b))
+		end} end
+	]}.
 
 
 -endif.
